87d378f197442b2914f1929e712e1563093b59b5
satwik-naik <naiksatwik7@gmail.com>
2024-06-15 15:32:19 +0530

    v-2

--- ftpServer.js
+++ ftpServer.js
@@ -1,11 +1,30 @@
 const express =require("express")
 const mongoose=require("mongoose")
 const cors=require('cors');
+const multer=require("multer")
+
 
 require("./models/user")
+
+
+
 const app=express()
 app.use(express.json())
 app.use(cors())
+app.use(express.urlencoded({extended:false}))
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+      cb(null,`${Date.now()}-${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage: storage })
+
 
 let mongooseUrl="mongodb+srv://codeguy181:satwik3840@cluster0.bopkk7s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
@@ -51,7 +70,7 @@ app.post('/login', async (req, res) => {
       const person = await User.findOne({ "email":email });
 
       if (!person) {
-          return res.status(404).json({ error: 'User not found' });
+          return  res.status(404).json({ error: 'User not found' });
       }
 
       if (person.password === password) {
@@ -65,7 +84,12 @@ app.post('/login', async (req, res) => {
   }
 });
   
+app.post("/upload" ,upload.single("ftp_files"),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file)
 
+    return res.redirect("http://localhost:3000/home")
+})
 
 app.listen(5000,()=>{
   console.log("server is running ....")
